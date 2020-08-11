5ce3005bf95242ec51f5ca9a2331e83d16f0524e
Alfonso <strotgen@gmail.com>
2020-08-11 14:51:52 -0500

    Array order.

--- src/Client.php
+++ src/Client.php
@@ -8,10 +8,10 @@ class Client extends DocuSignClient
 {
     public function __construct(array $params = [])
     {
-        $params['host'] =  $params['host'] ?? config('docusign.username');
-        $params['username'] =  $params['username'] ?? config('docusign.password');
-        $params['password'] =  $params['password'] ?? config('docusign.integrator_key');
-        $params['integrator_key'] =  $params['integrator_key'] ?? config('docusign.host');
+        $params['host'] =  $params['host'] ?? config('docusign.host');
+        $params['username'] =  $params['username'] ?? config('docusign.username');
+        $params['password'] =  $params['password'] ?? config('docusign.password');
+        $params['integrator_key'] =  $params['integrator_key'] ?? config('docusign.integrator_key');
 
         parent::__construct($params);
     }
